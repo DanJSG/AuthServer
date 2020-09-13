98c0f5ca296ead68d8854dab2d1215512d577f02
DanJSG <jackson.daniel365@gmail.com>
2020-09-13 15:17:25 +0100

    Enabled query strings to be copied between routes to allow register page to complete sign in [OGDEV-129]

--- account-screen/src/components/LoginPage/Forms/LoginForm.jsx
+++ account-screen/src/components/LoginPage/Forms/LoginForm.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { buildQueryStringFromObject } from '../../../services/querystringmanipulator';
 
 function LoginForm(props) {
+
+    const [redirectPath] = useState(buildQueryStringFromObject("/oauth2/register", props.urlParams))
+
     return (
         <div className="row align-items-center justify-content-center h-100" style={{ backgroundColor: "#f3f5f744" }}>
             <div className="col-3 border shadow-sm rounded px-5 py-3 bg-white" >
@@ -20,7 +24,7 @@ function LoginForm(props) {
                         </div>
                         <div className="form-group">
                             <label className="form-text">
-                                Not got an account yet? <Link className="link text-decoration-none" to="/oauth2/register">Sign up here.</Link>
+                                Not got an account yet? <Link className="link text-decoration-none" to={redirectPath}>Sign up here.</Link>
                             </label>
                         </div>
                     </form>
