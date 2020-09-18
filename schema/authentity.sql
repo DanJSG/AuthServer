4ee0a4c36c10f9c6850510423c4f895332f6edfe
DanJSG <jackson.daniel365@gmail.com>
2020-09-18 17:03:46 +0100

    Added updated database schema [OGDEV-131]

--- schema/authentity.sql
+++ schema/authentity.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: localhost    Database: authentity
+-- Host: 127.0.0.1    Database: authentity
 -- ------------------------------------------------------
 -- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,34 +16,53 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `auth.apps`
+-- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `auth.apps`;
+DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth.apps` (
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `accounts` (
+  `id` bigint(12) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `email` varchar(254) NOT NULL,
+  `datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` binary(60) NOT NULL,
+  `datechanged` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idusers.account_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1664 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `apps`
+--
+
+DROP TABLE IF EXISTS `apps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `apps` (
   `client_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `redirect_uri` varchar(500) NOT NULL,
   `client_secret` varchar(255) NOT NULL,
   `access_token_secret` varchar(255) NOT NULL,
   `associated_account_id` bigint(12) unsigned zerofill NOT NULL,
   PRIMARY KEY (`client_id`),
   UNIQUE KEY `client_id_UNIQUE` (`client_id`),
-  UNIQUE KEY `endpoint_UNIQUE` (`redirect_uri`),
   KEY `id_idx` (`associated_account_id`),
-  CONSTRAINT `associated_account_id` FOREIGN KEY (`associated_account_id`) REFERENCES `users.accounts` (`id`)
+  CONSTRAINT `associated_account_id` FOREIGN KEY (`associated_account_id`) REFERENCES `accounts` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `auth.challenge`
+-- Table structure for table `challenges`
 --
 
-DROP TABLE IF EXISTS `auth.challenge`;
+DROP TABLE IF EXISTS `challenges`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth.challenge` (
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `challenges` (
   `client_id` varchar(255) NOT NULL,
   `code_challenge` varchar(87) NOT NULL,
   `expires` datetime NOT NULL,
@@ -53,13 +72,63 @@ CREATE TABLE `auth.challenge` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `auth.codes`
+-- Table structure for table `chatmembers`
 --
 
-DROP TABLE IF EXISTS `auth.codes`;
+DROP TABLE IF EXISTS `chatmembers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth.codes` (
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `chatmembers` (
+  `id` bigint(12) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `chatid` varchar(36) NOT NULL,
+  `memberid` bigint(12) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `memberid_idx` (`memberid`),
+  KEY `chatid_idx` (`chatid`),
+  CONSTRAINT `chatid` FOREIGN KEY (`chatid`) REFERENCES `chats` (`chatid`),
+  CONSTRAINT `memberid` FOREIGN KEY (`memberid`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=418 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `chats`
+--
+
+DROP TABLE IF EXISTS `chats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `chats` (
+  `chatid` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`chatid`),
+  UNIQUE KEY `chatid_UNIQUE` (`chatid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `chatsfull`
+--
+
+DROP TABLE IF EXISTS `chatsfull`;
+/*!50001 DROP VIEW IF EXISTS `chatsfull`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `chatsfull` AS SELECT 
+ 1 AS `chatid`,
+ 1 AS `name`,
+ 1 AS `id`,
+ 1 AS `username`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `codes`
+--
+
+DROP TABLE IF EXISTS `codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `codes` (
   `client_id` varchar(255) NOT NULL,
   `code` varchar(255) NOT NULL,
   `expires` datetime NOT NULL,
@@ -69,13 +138,31 @@ CREATE TABLE `auth.codes` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `auth.tokens`
+-- Table structure for table `info`
 --
 
-DROP TABLE IF EXISTS `auth.tokens`;
+DROP TABLE IF EXISTS `info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth.tokens` (
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `info` (
+  `index` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(12) unsigned zerofill NOT NULL,
+  `displayname` varchar(24) DEFAULT NULL,
+  `bio` varchar(254) DEFAULT NULL,
+  PRIMARY KEY (`index`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1047 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tokens`
+--
+
+DROP TABLE IF EXISTS `tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tokens` (
   `id` bigint(16) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `client_id` varchar(255) NOT NULL,
   `cookieToken` varchar(255) NOT NULL,
@@ -84,60 +171,41 @@ CREATE TABLE `auth.tokens` (
   `expired` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1099 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2157 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users.accounts`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users.accounts`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users.accounts` (
-  `id` bigint(12) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `email` varchar(254) NOT NULL,
-  `datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `password` binary(60) NOT NULL,
-  `datechanged` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idusers.account_UNIQUE` (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=623 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users.info`
---
-
-DROP TABLE IF EXISTS `users.info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users.info` (
-  `index` bigint NOT NULL AUTO_INCREMENT,
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
   `id` bigint(12) unsigned zerofill NOT NULL,
-  `displayname` varchar(24) DEFAULT NULL,
-  `bio` varchar(254) DEFAULT NULL,
-  PRIMARY KEY (`index`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `users.accounts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users.links`
---
-
-DROP TABLE IF EXISTS `users.links`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users.links` (
-  `id` bigint(12) unsigned zerofill NOT NULL,
-  `linkid` bigint(12) unsigned zerofill NOT NULL,
+  `username` varchar(24) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `chatsfull`
+--
+
+/*!50001 DROP VIEW IF EXISTS `chatsfull`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`localDev`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `chatsfull` AS select `m`.`chatid` AS `chatid`,`c`.`name` AS `name`,`u`.`id` AS `id`,`u`.`username` AS `username` from ((`chatmembers` `m` left join `chats` `c` on((`c`.`chatid` = `m`.`chatid`))) left join `users` `u` on((`m`.`memberid` = `u`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -148,4 +216,4 @@ CREATE TABLE `users.links` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-22 15:20:05
+-- Dump completed on 2020-09-18 17:03:31
