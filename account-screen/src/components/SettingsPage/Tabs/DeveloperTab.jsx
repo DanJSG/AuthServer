72676d97f74c14740ce527e2c41bc71acf478ca2
DanJSG <jackson.daniel365@gmail.com>
2020-09-18 23:33:59 +0100

    Added delete button, fixed API spam, added map index for list key [OGDEV-133]

--- account-screen/src/components/SettingsPage/Tabs/DeveloperTab.jsx
+++ account-screen/src/components/SettingsPage/Tabs/DeveloperTab.jsx
@@ -19,14 +19,14 @@ function DeveloperTab(props) {
             const apps = await getApps(localStorage.getItem("acc.tok"));
             setApplications(apps);
         }
-        fetchApps();
+        if (applications == null)
+            fetchApps();
     })
 
     return (
         <div className="col-10 w-100">
             <div className="ml-3 w-100">
                 <h1>Developer Settings</h1>
-                <p className="text-danger"><i><b>None of this has yet been implemented in the backend and so is not currently functional.</b></i></p>
                 <hr />
                 <div>
                     <h3 className="mb-3">Register Application</h3>
@@ -51,13 +51,16 @@ function DeveloperTab(props) {
                             <p>You have not currently registered any applications.</p>
                             :
                             <ul className="list-group">
-                                {applications.map(app =>
-                                    <div key={app.name} className="list-group-item border-0 selectable rounded">
+                                {applications.map((app, index) =>
+                                    <div key={index} className="list-group-item border-0 selectable rounded">
                                         <div className="d-flex justify-content-between">
                                             <h5>{app.name}</h5>
-                                            <button className="btn btn-secondary px-4">Edit</button>
+                                            <button className="btn btn-secondary my-1" style={{ width: "7%" }}>Edit</button>
                                         </div>
-                                        <p>Callback URL: <a href={app.redirectUri}>{app.redirectUri}</a></p>
+                                        <div className="d-flex justify-content-between">
+                                            <p>Callback URL: <a href={app.redirectUri}>{app.redirectUri}</a></p>
+                                            <button className="btn btn-danger my-1" style={{ width: "7%" }}>Delete</button>
+                                        </div>
                                     </div>
                                 )}
                             </ul>
